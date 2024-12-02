9c66cb917b5cbb3086db819a2514ace3690c5adb
Successdanesy <successdanesy@gmail.com>
2024-12-02 09:58:44 +0100

    this verison does not submit orders to the database sha

--- kitchen.php
+++ kitchen.php
@@ -97,7 +97,6 @@ $orders = fetchOrders($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kitchen Orders</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="kitchen.css">
 </head>
 <body>
