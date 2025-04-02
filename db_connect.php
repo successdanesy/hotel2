c0bff807584d008ddfb9a5e0029d90a31b3c39a3
Success <stealph14@gmail.com>
2025-04-02 19:53:50 +0100

    updated the db_connect to use infinity free hosting service

--- db_connect.php
+++ db_connect.php
@@ -1,33 +1,14 @@
 <?php
-$servername = "zpfp07ebhm2zgmrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-$username = "hy7x3wktu25w0pae";
-$password = "ak9uacoz59zlwhiv";
-$database = "l1oe4lyo2lg1i3dg";
-$port = 3306; // MySQL default port
+$servername = "sql109.infinityfree.com"; // MySQL Hostname
+$username = "if0_38659407"; // MySQL Username
+$password = "YABAM509am"; // MySQL Password
+$database = "if0_38659407_XXX"; // MySQL Database Name
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database, $port);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-
-
-
-
-<!-- //Database connection details
-//$host = 'localhost';  // Host name (usually 'localhost')
-//$username = 'root';   // Your database username (typically 'root' for local dev)
-//$password = '';       // Your database password (empty for XAMPP by default)
-//$dbname = 'project';  // The name of the database
-
-// Create a connection to the database
-//$conn = new mysqli($host, $username, $password, $dbname);
-
-// Check if the connection is successful
-//if ($conn->connect_error) {
-    //die("Connection failed: " . $conn->connect_error);
-//}
-// echo "Connected successfully"; // You can keep this for debugging purposes -->
