e0cc05a6beeab8d41b23a57b6052d67c56100d8a
Success <stealph14@gmail.com>
2025-04-02 09:57:34 +0100

    updated to carry jawsdb url

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,34 @@
 <?php
-// Database connection details
-$host = 'localhost';  // Host name (usually 'localhost')
-$username = 'root';   // Your database username (typically 'root' for local dev)
-$password = '';       // Your database password (empty for XAMPP by default)
-$dbname = 'project';  // The name of the database
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
 
-// Create a connection to the database
-$conn = new mysqli($host, $username, $password, $dbname);
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
 
-// Check if the connection is successful
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// echo "Connected successfully"; // You can keep this for debugging purposes
+//echo "Connection was successfully established!";
 ?>
+
+
+
+<!-- //Database connection details
+//$host = 'localhost';  // Host name (usually 'localhost')
+//$username = 'root';   // Your database username (typically 'root' for local dev)
+//$password = '';       // Your database password (empty for XAMPP by default)
+//$dbname = 'project';  // The name of the database
+
+// Create a connection to the database
+//$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check if the connection is successful
+//if ($conn->connect_error) {
+    //die("Connection failed: " . $conn->connect_error);
+//}
+// echo "Connected successfully"; // You can keep this for debugging purposes -->
